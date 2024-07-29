e89f87776bd836aff05d0cc57343960996ff2e2c
Tobis <tobias@brodec.eu>
2024-07-29 18:23:20 +0200

    Change server project

---
+++ Server/Server/Controllers/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
