55dd847030d53fe1f4d31eb2e727962a930c4fbe
Tobis <tobias@brodec.eu>
2025-03-21 22:16:02 +0100

    Fully working RSA

---
+++ ClientMaui/ClientMaui/Cryptography/SelfImplemented/RSA/BigIntegerExtension.cs
@@ -0,0 +1,114 @@
+﻿using System.Numerics;
+
+static class BigIntegerExtension
+{
+    // https://stackoverflow.com/questions/3432412/calculate-square-root-of-a-biginteger-system-numerics-biginteger
+    public static BigInteger NewtonPlusSqrt(this BigInteger x)
+    {
+        if (x < 144838757784765629)    // 1.448e17 = ~1<<57
+        {
+            uint vInt = (uint)Math.Sqrt((ulong)x);
+            if ((x >= 4503599761588224) && ((ulong)vInt * vInt > (ulong)x))  // 4.5e15 =  ~1<<52
+            {
+                vInt--;
+            }
+            return vInt;
+        }
+
+        var xAsDub = (double)x;
+        switch (xAsDub)
+        {
+            //  long.max*long.max
+            case < 8.5e37:
+                {
+                    var vInt = (ulong)Math.Sqrt(xAsDub);
+                    BigInteger v = (vInt + ((ulong)(x / vInt))) >> 1;
+                    return (v * v <= x) ? v : v - 1;
+                }
+            case < 4.3322e127:
+                {
+                    var v = (BigInteger)Math.Sqrt(xAsDub);
+                    v = (v + (x / v)) >> 1;
+                    if (xAsDub > 2e63)
+                    {
+                        v = (v + (x / v)) >> 1;
+                    }
+                    return (v * v <= x) ? v : v - 1;
+                }
+        }
+
+        //int xLen = (int)x.GetBitLength();
+        var xLen = GetBitLengthFallback(x);
+        var wantedPrecision = (xLen + 1) / 2;
+        var xLenMod = xLen + (xLen & 1) + 1;
+
+        //////// Do the first Sqrt on hardware ////////
+        var tempX = (long)(x >> (xLenMod - 63));
+        var tempSqrt1 = Math.Sqrt(tempX);
+        var valLong = (ulong)BitConverter.DoubleToInt64Bits(tempSqrt1) & 0x1fffffffffffffL;
+        if (valLong == 0)
+        {
+            valLong = 1UL << 53;
+        }
+
+        ////////  Classic Newton Iterations ////////
+        var val = ((BigInteger)valLong << 52) + ((x >> (xLenMod - (3 * 53))) / valLong);
+        var size = 106;
+        for (; size < 256; size <<= 1)
+        {
+            val = (val << (size - 1)) + ((x >> (xLenMod - (3 * size))) / val);
+        }
+
+        if (xAsDub > 4e254) // 4e254 = 1<<845.76973610139
+        {
+            var numOfNewtonSteps = BitOperations.Log2((uint)(wantedPrecision / size)) + 2;
+
+            //////  Apply Starting Size  ////////
+            var wantedSize = (wantedPrecision >> numOfNewtonSteps) + 2;
+            var needToShiftBy = size - wantedSize;
+            val >>= needToShiftBy;
+            size = wantedSize;
+            do
+            {
+                ////////  Newton Plus Iterations  ////////
+                var shiftX = xLenMod - (3 * size);
+                var valSqrd = (val * val) << (size - 1);
+                var valSU = (x >> shiftX) - valSqrd;
+                val = (val << size) + (valSU / val);
+                size *= 2;
+            } while (size < wantedPrecision);
+        }
+
+        /////// There are a few extra digits, let's save them ///////
+        var oversidedBy = size - wantedPrecision;
+        var saveDroppedDigitsBI = val & ((BigInteger.One << oversidedBy) - 1);
+        var downby = (oversidedBy < 64) ? (oversidedBy >> 2) + 1 : (oversidedBy - 32);
+        var saveDroppedDigits = (ulong)(saveDroppedDigitsBI >> downby);
+
+
+        ////////  Shrink result to wanted Precision  ////////
+        val >>= oversidedBy;
+
+
+        ////////  Detect a round-up ////////
+        if ((saveDroppedDigits == 0) && (val * val > x))
+        {
+            val--;
+        }
+
+        return val;
+    }
+
+    public static int GetBitLengthFallback(BigInteger x) // only support x > 0
+    {
+        var bytes = x.ToByteArray();
+        var msb = bytes[^1];
+        var msbBits = 0;
+        while (msb != 0)
+        {
+            msb >>= 1;
+            msbBits++;
+        }
+        return ((bytes.Length - 1) * 8) + msbBits;
+    }
+}
--- ClientMaui/ClientMaui/Cryptography/SelfImplemented/RSA/SelfRSA.cs
+++ ClientMaui/ClientMaui/Cryptography/SelfImplemented/RSA/SelfRSA.cs
@@ -1,67 +1,191 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
+using System.Security.Cryptography;
 
-using HealthKit;
 
 namespace ClientMaui.Cryptography.SelfImplemented.RSA
 {
     internal class SelfRSA
     {
-        public static List<int> splitNumberToSmallerPowers(int number)
+        static List<BigInteger> splitNumberToSmallerPowers(BigInteger number)
         {
-            var result = new List<int>();
+            var result = new List<BigInteger>();
             var remainder = number;
 
             while (remainder > 0)
             {
-                var mathLog = Math.Log(remainder, 2);
+                var mathLog = BigInteger.Log(remainder, 2);
                 var power = (int)Math.Floor(mathLog);
-                var value = (int)Math.Pow(2, power);
+                var value = BigInteger.Pow(2, power);
                 result.Add(value);
                 remainder -= value;
             }
             return result;
 
         }
-        public static int modPow(int number, int power, int mod)
+        static BigInteger modPow(BigInteger number, BigInteger power, BigInteger mod)
         {
             var powers = splitNumberToSmallerPowers(power);
-            var lookupTable = new Dictionary<int, int>();
+            var lookupTable = new Dictionary<BigInteger, BigInteger>();
             if (powers.Contains(1))
             {
                 lookupTable.Add(1, number % mod);
             }
 
-            for (var i = 2; i <= powers.Max(); i*=2)
+            for (var i = new BigInteger(2); i <= powers.Max(); i *= 2)
             {
-                
-                lookupTable.Add(i, (int)Math.Pow(lookupTable[i/2], 2) % mod);
-                
+
+                lookupTable.Add(i, BigInteger.Pow(lookupTable[i / 2], 2) % mod);
+
             }
             var result = powers.Aggregate(BigInteger.One, (current, item) => current * lookupTable[item]);
 
-            return (int)(result % mod);
+            return result % mod;
         }
-        static bool IsPrime(int number)
+        static BigInteger GenerateRandomBigInteger(int bitLength)
         {
-            if (number < 2)
-                return false;
-            var stop = Math.Sqrt(number);
-            for (var i = 2; i <= stop; i++)
+            int byteLength = (bitLength + 7) / 8;
+            byte[] bytes = new byte[byteLength];
+            RandomNumberGenerator.Fill(bytes);
+
+            // Zajistíme, že nejvyšší bit je nastaven, aby číslo mělo správnou bitovou délku
+            bytes[^1] |= 0x80;
+
+            return new BigInteger(bytes, isUnsigned: true, isBigEndian: true);
+        }
+
+        // Miller-Rabin test na prvočíselnost
+        static bool IsPrime(BigInteger n, int k = 10)
+        {
+            if (n < 2) return false;
+            if (n == 2 || n == 3) return true;
+            if (n % 2 == 0) return false;
+
+            // Rozložíme n-1 na d * 2^r
+            BigInteger d = n - 1;
+            int r = 0;
+            while (d % 2 == 0)
             {
-                if (number % i == 0)
-                    return false;
+                d /= 2;
+                r++;
+            }
+
+            // Opakujeme test k-krát
+            for (int i = 0; i < k; i++)
+            {
+                BigInteger a = (GenerateRandomBigInteger(n.ToByteArray().Length * 8) % (n - 3)) + 2;
+                BigInteger x = BigInteger.ModPow(a, d, n);
+                if (x == 1 || x == n - 1) continue;
+
+                bool composite = true;
+                for (int j = 0; j < r - 1; j++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == n - 1)
+                    {
+                        composite = false;
+                        break;
+                    }
+                }
+                if (composite) return false;
             }
             return true;
         }
 
-        static int GenerateRandomPrime(BigInteger min, BigInteger max)
+        // Generování prvočísla o dané bitové délce
+        static BigInteger GeneratePrime(int bitLength)
         {
+            BigInteger prime;
+            do
+            {
+                prime = GenerateRandomBigInteger(bitLength) | 1; // Ujistíme se, že číslo je liché
+            } while (!IsPrime(prime));
+            return prime;
         }
+
+        //Rozšířený Euklidův algoritmus
+        static BigInteger ModInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m, t, q;
+            BigInteger x0 = 0, x1 = 1;
+
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                q = a / m;
+                t = m;
+
+                m = a % m;
+                a = t;
+                t = x0;
+
+                x0 = x1 - (q * x0);
+                x1 = t;
+            }
+
+            if (x1 < 0)
+                x1 += m0;
+
+            return x1;
+        }
+        static BigInteger gcd(BigInteger a, BigInteger b)
+        {
+            while (b != 0)
+            {
+                (b, a) = (a % b, b);
+            }
+            return a;
+        }
+
+        public static (BigInteger E, BigInteger N, BigInteger D) GenerateRSAKeyPair(int keySize = 1024)
+        {
+            GC.TryStartNoGCRegion(keySize * 1000 * 1000, keySize);
+            var size = keySize / 2;
+            var P = GeneratePrime(size);
+            var Q = GeneratePrime(size);
+            while (P == Q)
+            {
+                Q = GeneratePrime(size);
+            }
+            var N = P * Q;
+            var phi = (P - 1) * (Q - 1);
+            var E = new BigInteger(65537);
+            var D = new BigInteger(0);
+            while (true)
+            {
+                if (gcd(E, phi) == 1)
+                {
+                    D = ModInverse(E, phi);
+                    break;
+                }
+                E++;
+            }
+            GC.EndNoGCRegion();
+            return (E, N, D);
+        }
+        static BigInteger EncryptCharacter(int message, BigInteger E, BigInteger N)
+        {
+            return modPow(message, E, N);
+        }
+        static BigInteger DecryptCharacter(BigInteger message, BigInteger D, BigInteger N)
+        {
+            return modPow(message, D, N);
+        }
+        public static List<BigInteger> Encrypt(string message, BigInteger E, BigInteger N)
+        {
+            var result = message.Select(character => EncryptCharacter(character, E, N)).ToList();
+            return result;
+        }
+        public static string Decrypt(List<BigInteger> message, BigInteger D, BigInteger N)
+        {
+            var result = message.Select(character => DecryptCharacter(character, D, N)).ToList();
+
+            var bytes = result.Select(bi => (char)(int)bi).ToArray();
+            return new string(bytes);
+        }
+
+
+
 
     }
 }
